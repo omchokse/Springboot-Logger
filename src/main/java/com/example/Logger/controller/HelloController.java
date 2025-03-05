39e0a39be2f9a928ba81b8b8690213712b5233fa
Om Chokse <omchokse30@gmail.com>
2025-03-05 23:40:28 +0530

    Implemented UC2: GET request with query parameter

--- src/main/java/com/example/Logger/controller/HelloController.java
+++ src/main/java/com/example/Logger/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.Logger.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +13,9 @@ public class HelloController {
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
+    @GetMapping("/query")
+    public String sayHelloWithQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
 }
